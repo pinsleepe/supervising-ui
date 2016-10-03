bebabf8cb6a2fc26059f7bd2b323e7681524bbe2
mobrocka <mobrocka@ska.ac.za>
2016-10-03 11:08:34 +0200

    house keeping

--- supervising_gui/app.py
+++ supervising_gui/app.py
@@ -26,6 +26,7 @@ from datetime import datetime
 import urllib
 
 from flask import Flask, render_template, request, abort, send_file, redirect, Response
+
 app = Flask(__name__)
 
 # Constants
@@ -33,10 +34,10 @@ DB_FILE = "db.sqlite"
 SETTINGS_FILE = "settings.json"
 LOGS_FILE = "logs.log"
 CREATE_TABLE_STMT = "CREATE TABLE IF NOT EXISTS data (" \
-    "url text PRIMARY KEY, " \
-    "label text, " \
-    "last_modified datetime DEFAULT current_timestamp, " \
-    " UNIQUE (url) ON CONFLICT IGNORE )" # Ignore it to preserve previous labels
+                    "url text PRIMARY KEY, " \
+                    "label text, " \
+                    "last_modified datetime DEFAULT current_timestamp, " \
+                    " UNIQUE (url) ON CONFLICT IGNORE )"  # Ignore it to preserve previous labels
 INSERT_STMT = "INSERT INTO data VALUES (?, ?, ?)"
 UPDATE_STMT = "UPDATE data SET label=?, last_modified=datetime() WHERE url=?"
 SELECT_UNLABELLED = "SELECT * FROM data WHERE label IS NULL"
@@ -44,8 +45,8 @@ SELECT_LABELLED = "SELECT * FROM data WHERE label IS NOT NULL"
 GET_STMT = "SELECT * FROM data WHERE url = ?"
 LOG_LEVEL = logging.DEBUG
 
+service = None  # will be initialized from CLI args
 
-service = None # will be initialized from CLI args
 
 @app.route("/")
 def webpage():
@@ -65,12 +66,14 @@ def webpage():
     }
     return render_template('index.html', **data)
 
+
 @app.route("/proxy")
 def document():
     url = request.args.get('url')
     if not url or not os.path.exists(url):
         return abort(400, "File %s not found " % url)
     return send_file(url)
+
 
 @app.route("/update", methods=['POST'])
 def update():
@@ -89,16 +92,19 @@ def update():
     else:
         return abort(400, "Failed... No records updated")
 
+
 @app.route("/settings")
 def get_settings():
     return json.dumps(service.settings)
+
 
 @app.route("/download.csv")
 def download():
     recs = service.query_recs(SELECT_LABELLED + " ORDER BY last_modified DESC", first_only=False)
     recs = map(lambda r: "\t".join([r['last_modified'], r['url'], r['label']])
-                            + "\n", recs)
+                         + "\n", recs)
     return Response(recs, mimetype='text/csv')
+
 
 def get_next(url=None):
     next_rec = service.get_record(url)
@@ -106,14 +112,14 @@ def get_next(url=None):
     template_name = '%s.html' % service.settings['type']
     data_url = url if url.startswith('http') else "/proxy?url=%s" % urllib.quote(next_rec['url'])
     data = {
-        'data_url' : data_url,
+        'data_url': data_url,
         'url': url,
         'task': service.settings['task']
     }
     return render_template(template_name, **data)
 
-class DbService(object):
 
+class DbService(object):
     def __init__(self, workdir, input_file):
         self.workdir = workdir
         print("Work Dir : %s" % workdir)
@@ -128,7 +134,7 @@ class DbService(object):
         self.log.basicConfig(filename=logs_fn, level=LOG_LEVEL)
         with open(settings_fn) as f:
             self.settings = json.load(f)
-            self.log.debug("Loaded the settings : %s" % (self.settings))
+            self.log.debug("Loaded the settings : %s" % self.settings)
         self.db = self.connect_db()
         self.log.info("Work Dir %s" % workdir)
         if input_file:
@@ -146,11 +152,13 @@ class DbService(object):
         db_file = os.path.join(self.workdir, DB_FILE)
         self.log.info("Connecting to database file at %s" % db_file)
         db = sqlite3.connect(db_file, check_same_thread=False)
-        def dict_factory(cursor, row): # map tuples to dictionary with column names
+
+        def dict_factory(cursor, row):  # map tuples to dictionary with column names
             d = {}
             for idx, col in enumerate(cursor.description):
                 d[col[0]] = row[idx]
             return d
+
         db.row_factory = dict_factory
         cursor = db.cursor()
         cursor.execute(CREATE_TABLE_STMT)
@@ -163,7 +171,7 @@ class DbService(object):
         cursor = self.db.cursor()
         for url in urls:
             values = (url, None, datetime.now())
-             # assumption: if rec exists, DB will IGNORE IT
+            # assumption: if rec exists, DB will IGNORE IT
             res = cursor.execute(INSERT_STMT, values)
             count += 1
         self.db.commit()
@@ -181,7 +189,7 @@ class DbService(object):
 
     def get_next_unlabelled(self):
         return self.query_recs(SELECT_UNLABELLED + " ORDER BY RANDOM() LIMIT 1",
-                                first_only=True)
+                               first_only=True)
 
     def get_record(self, url):
         return self.db.execute(GET_STMT, (url,)).fetchone()
@@ -206,9 +214,10 @@ class DbService(object):
             self.db.commit()
             self.db = None
 
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Web UI for Labeling images")
-    parser.add_argument("-i", "--input", help="Path to to input file which has list of paths, one per line. (Optional)")
+    parser.add_argument("-i", "--input", help="Path to input file which has list of paths, one per line. (Optional)")
     parser.add_argument("-w", "--work-dir", help="Work Directory. (Required)", required=True)
     parser.add_argument("-p", "--port", type=int, help="Bind port. (Optional)", default=8080)
     args = vars(parser.parse_args())
